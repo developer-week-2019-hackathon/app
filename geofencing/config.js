7aa4186f971ca8d0de65c448fe70601187eb4da6
Ricardo Piro-Rael <ricardo@hint.com>
2019-02-23 21:14:43 -0800

    Added geofencing

---
+++ geofencing/config.js
@@ -0,0 +1,3 @@
+var apiKey = 'NAQ30angVoAKSIHgaB5aMjhHZjVv8q2g';
+var geofencingAdminKey = 'WCdBCE12XG8slb5kpj1E4fO0UsslcFVsI86mCRRIxu85h3DZ';
+var geofencingProjectId = 'f3d2bb2c-4bc6-4957-a8a0-14049069e83d';
